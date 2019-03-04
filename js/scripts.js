af1e9d2a4eb41d838177f7df3646b1112b0f6d59
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-03-04 17:02:11 +0300

    remove spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
-function pig dice(){
-  var player1 = parseInt(document.getElementsByClassName('player1').value);
-  var player2 =parseInt(document.getElementsByClassName('player2').value);
-  var player = [player1,player2];
+// business logic
+// function pig Dice(player1, player2) {
+//   this.player1 = player1;
+//   this.player2 = player2;
+// }
+
+
+
+
+// user interface logic
+// $(document).ready(function() {
+//   $("form#new-player").submit(function(event) {
+//     event.preventDefault();
+//     var inputtedplayer1 = $("input#new-player-1").val();
+//     var inputtedplayer2 = $("input#new-player-2").val();
+//
+//     var newPlayer = new Player(inputtedplayer1, inputtedplayer2);
+//
+//     $("input#new-player-1").val("");
+//     $("input#new-player-2").val("");
+//   });
+// });
+function check(){
+
+
+function Dice(turnScore, totalScore) {
+  this.turnScore = turnScore;
+  this.totalScore=totalScore;
 }
+Dice.prototype.Diceroll=function(){
+  var diceValues=Math.floor(Math.random)()*6 ) + 1;
+  if (diceValues === 1){
+  turnScore=0;
+}
+  else {
+    turnScore=turnScore+diceValues;
+  };
+}
+document.getElementById('new').innerHTML=turnScore;
+});
